e1ce676bb622ff81b6047eaef254170012d7ff28
yuy.hirohiro@gmail.com <yuy.hirohiro@gmail.com>
2021-06-03 23:12:37 +0900

    add sample

--- src/case.scala
+++ src/case.scala
@@ -1,6 +1,6 @@
 abstract class Expr
 case class Var(name: String) extends Expr
-case class Number(num: Int) extends Expr
+case class Number(num: Int) extends Expr 
 case class UnOp(op: String, arg: Expr) extends Expr
 case class BinOp(op: String, left: Expr,right: Expr) extends Expr
 
@@ -12,7 +12,6 @@ def simplifyTop(expr: Expr): Expr = expr match {
   case _ => expr
 }
 
-// ワイルドカード
 def wildCard(expr: Expr) = expr match {
   case BinOp(op, left, right) => println(s"$op,  $left,  $right")
   case _ => println("default is called")
@@ -50,8 +49,22 @@ def tuple(expr: (Expr, Expr, Expr)) = expr match {
 
 def withType(x: Any) = x match {
   case s: String => println(s)
-  case s: Map[_, _] => println(s)
+ case s: Map[_, _] => println(s)
+  // the type test for Map[Int, Int] cannot be checked at runtime
+  // case s: Map[Int, Int] => println(s)
   case x => println(s"$x")
+}
+
+def guard(expr: Expr) = expr match {
+  // error
+  //case BinOp("+", x, x) => println("error")
+  case BinOp("+", x, y) if x == y => println(s"$x, $y")
+  case _ => println("default")
+}
+
+def withDefault: Option[Int] => Int = {
+  case Some(x) => x
+  case None => 0
 }
 
 @main def main() = {
@@ -70,5 +83,10 @@ def withType(x: Any) = x match {
   withType(1)
   withType(Map("key1" -> 111, "key2" -> 222))
   withType(List(1, 2))
+
+  guard(BinOp("+", Var("x"), Var("x")))
+  
+  println(withDefault(Some(10)))
+
 }
 
