b55bd7fa620eb4d986c98e2c32668cb7abdff73b
yuy.hirohiro@gmail.com <yuy.hirohiro@gmail.com>
2021-06-06 01:41:29 +0900

    add cats effects example

---
+++ cats-effects/src/main/scala/example/FiberSample.scala
@@ -0,0 +1,71 @@
+package example
+
+import cats.effect._
+import cats.effect.std.Console
+import cats.syntax.all._
+import example.FiberSample.{State, consumer, producer}
+
+import scala.collection.immutable.Queue
+
+
+object FiberSample {
+
+  case class State[F[_], A](queue: Queue[A], tracker: Queue[Deferred[F, A]])
+
+  object State {
+    def empty[F[_], A]: State[F, A] = State(Queue.empty, Queue.empty)
+  }
+
+  def producer[F[_] : Sync : Console](id: Int, counterR: Ref[F, Int], stateR: Ref[F, State[F, Int]]): F[Unit] = {
+    def offer(i: Int): F[Unit] =
+      stateR.modify {
+        case State(queue, takers) if takers.nonEmpty ⇒
+          val (taker, rest) = takers.dequeue
+          State(queue, rest) -> taker.complete(i).void
+        case State(queue, takers) =>
+          State(queue.enqueue(i), takers) -> Sync[F].unit
+      }.flatten
+
+    for {
+      i <- counterR.getAndUpdate(_ + 1)
+      _ <- offer(i)
+      _ <- if (i % 10000 == 0) Console[F].println(s"Producer $id hase reached $i items") else Sync[F].unit
+      _ <- producer(id, counterR, stateR)
+    } yield ()
+  }
+
+
+  def consumer[F[_] : Async : Console](id: Int, stateR: Ref[F, State[F, Int]]): F[Unit] = {
+    val take: F[Int] =
+      Deferred[F, Int].flatMap { taker =>
+        stateR.modify {
+          case State(queue, takers) if queue.nonEmpty =>
+            val (i, rest) = queue.dequeue
+            State(rest, takers) -> Async[F].pure(i)
+          case State(queue, takers) =>
+            State(queue, takers.enqueue(taker)) -> taker.get
+        }.flatten
+      }
+
+    for {
+      i <- take
+      _ <- if (i % 10000 == 0) Console[F].println(s"  Consumed $id has reached $i items") else Async[F].unit
+      _ <- consumer(id, stateR)
+    } yield ()
+  }
+}
+
+object InefficientProducerConsumer extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] =
+    for {
+      stateR <- Ref.of[IO, State[IO, Int]](State.empty[IO, Int])
+      counterR <- Ref.of[IO, Int](0)
+      producers = List.range(1, 11).map(producer(_, counterR, stateR))
+      consumers = List.range(1, 11).map(consumer(_, stateR))
+      res <- (producers ++ consumers)
+        .parSequence.as(ExitCode.Success)
+        .handleErrorWith { t =>
+          Console[IO].errorln(s"Error caught: ${t.getMessage}").as(ExitCode.Error)
+        }
+    } yield res
+}
--- cats-effects/src/main/scala/example/FileCopyExample.scala
+++ cats-effects/src/main/scala/example/FileCopyExample.scala
@@ -1,36 +1,38 @@
 package example
 
-import cats.effect.{ExitCode, IO, IOApp, Resource}
+import cats.effect._
+import cats.syntax.all._
 import example.CopyFIle.copy
 
 import java.io._
+import scala.language.higherKinds
 
 object CopyFIle {
 
-  def inputStream(f: File): Resource[IO, FileInputStream] =
-    Resource.fromAutoCloseable(IO(new FileInputStream(f)))
+  def inputStream[F[_] : Sync](f: File): Resource[F, FileInputStream] =
+    Resource.fromAutoCloseable(Sync[F].delay(new FileInputStream(f)))
 
-  def outputStream(f: File): Resource[IO, FileOutputStream] =
-    Resource.fromAutoCloseable(IO(new FileOutputStream(f)))
+  def outputStream[F[_] : Sync](f: File): Resource[F, FileOutputStream] =
+    Resource.fromAutoCloseable(Sync[F].delay(new FileOutputStream(f)))
 
-  def inputOutputStreams(in: File, out: File): Resource[IO, (InputStream, OutputStream)] =
+  def inputOutputStreams[F[_] : Sync](in: File, out: File): Resource[F, (InputStream, OutputStream)] =
     for {
       inStream <- inputStream(in)
       outStream <- outputStream(out)
     } yield (inStream, outStream)
 
 
-  def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): IO[Long] =
+  def transmit[F[_] : Sync](origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): F[Long] =
     for {
-      amount <- IO.blocking(origin.read(buffer, 0, buffer.length))
-      count <- if (amount > -1) IO.blocking(destination.write(buffer, 0, amount)) >> transmit(origin, destination, buffer, acc + amount)
-      else IO.pure(acc)
+      amount <- Sync[F].blocking(origin.read(buffer, 0, buffer.length))
+      count <- if (amount > -1) Sync[F].blocking(destination.write(buffer, 0, amount)) >> transmit(origin, destination, buffer, acc + amount)
+      else Sync[F].pure(acc)
     } yield count
 
-  def transfer(origin: InputStream, destination: OutputStream): IO[Long] =
+  def transfer[F[_] : Sync](origin: InputStream, destination: OutputStream): F[Long] =
     transmit(origin, destination, new Array[Byte](1024 * 10), 0L)
 
-  def copy(origin: File, destination: File): IO[Long] =
+  def copy[F[_] : Sync](origin: File, destination: File): F[Long] =
     inputOutputStreams(origin, destination).use {
       case (in, out) => transfer(in, out)
     }
@@ -43,7 +45,7 @@ object Main extends IOApp {
       _ <- IO.unit
       orig = new File("./orig.txt")
       dest = new File("./dest.txt")
-      count <- copy(orig, dest)
+      count <- copy[IO](orig, dest)
       _ <- IO.print(s"$count bytes copied from ${orig.getPath} to ${dest.getPath}")
     } yield ExitCode.Success
 }
